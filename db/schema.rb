5c5a664a7946e8cf9f4b4568078eff71c87ffd5a
Dallie Hill <lenore776@gmail.com>
2015-09-07 16:49:41 -0400

    Devise and layout completed. Calendar and time popup complete.

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150902201320) do
+ActiveRecord::Schema.define(version: 20150903150040) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "events", force: :cascade do |t|
     t.string   "name"
     t.integer  "user_id"
     t.string   "reminder_type"
-    t.string   "date_due"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
+    t.datetime "date_time"
   end
 
   create_table "notifications", force: :cascade do |t|
@@ -47,7 +50,7 @@ ActiveRecord::Schema.define(version: 20150902201320) do
     t.string   "cellphone_number"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
