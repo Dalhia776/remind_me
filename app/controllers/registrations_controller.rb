a6481f9e4e5ac714a56f8030acaab0f53b81a217
Dallie Hill <lenore776@gmail.com>
2015-09-09 17:31:04 -0400

    "Changed buttons and some of the other stuff on views"

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,30 +1,5 @@
 class RegistrationsController < Devise::RegistrationsController
 
-  def create
-    @registration = User.new(sign_up_params)
-
-    respond_to do |format|
-      if @registration.save
-        format.html { redirect_to root_path, notice: 'Account was successfully created.' }
-      else
-        format.html { render :new }
-        format.json { render json: @registration.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-
-  def update
-    @registration = current_user
-  respond_to do |format|
-    if @registration.update(account_update_params)
-      format.html { redirect_to root_path, notice: 'Update was successful.' }
-    else
-      format.html { render :edit }
-      format.json { render json: @registration.errors, status: :unprocessable_entity }
-    end
-  end
-end
 
   private
 
