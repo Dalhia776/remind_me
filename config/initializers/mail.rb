f4719aa1e83a661f1d120a5103ac8930a474aff3
Dallie Hill <lenore776@gmail.com>
2015-09-17 11:56:48 -0400

    Changed email

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -3,12 +3,12 @@ ActionMailer::Base.register_interceptor(SendGrid::MailInterceptor)
   ActionMailer::Base.smtp_settings = {
     :address => 'smtp.sendgrid.net',
     :port => '25',
-    :domain => 'gmail.com',
+    :domain => 'dalhia.net',
     :authentication => :plain,
     :user_name => ENV['sendgrid_username'],
     :password => ENV['sendgrid_password']
   }
 
   SendGrid.configure do |config|
-    config.dummy_recipient = 'lenore776@gmail.com'
+    config.dummy_recipient = 'support@dalhia.net'
   end
